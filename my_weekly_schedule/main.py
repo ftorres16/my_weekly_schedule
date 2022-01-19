9dd79ee70fff232ea138cfee33fd6149dfc76d21
fabian <36959980+ftorres16@users.noreply.github.com>
2022-01-19 18:53:21 -0500

    raise error when plot without weekend but there are events

--- my_weekly_schedule/main.py
+++ my_weekly_schedule/main.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-import matplotlib.pyplot as plt
 import typer
 
+from my_weekly_schedule.models import WeekdayEnum
 from my_weekly_schedule.parse_txt import parse_txt
 from my_weekly_schedule.plot_schedule import plot_events
 
@@ -13,18 +13,24 @@ def main(
     input_: Path,
     show: bool = True,
     save_img: bool = True,
-    with_weekends: bool = False,
+    weekends: bool = True,
 ):
     """
     Plot weekly schedule schedule.
     """
     events = parse_txt(input_)
 
+    if not weekends and any(
+        event.day.value > WeekdayEnum.fri.value for event in events
+    ):
+        typer.echo("Attempted to plot without weekends, but there are weekend events.")
+        raise typer.Abort()
+
     out_path = input_.with_suffix(".png")
 
     plot_events(
         events,
-        with_weekends=with_weekends,
+        with_weekends=weekends,
         show=show,
         save_img=save_img,
         out_path=out_path,
